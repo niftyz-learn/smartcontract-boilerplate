5b60e38d6ab261d07c77770d5112f875c92a8f1a
turinglabs <sebastiano.cataudo@gmail.com>
2023-11-09 14:24:17 +0100

    updating factory

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 async function main() {
   const configs = JSON.parse(fs.readFileSync(process.env.CONFIG).toString())
   console.log('Deploying contract..')
-  const DemVesting = await hre.ethers.getContractFactory(configs.contract_name);
-  const contract = await DemVesting.deploy(...configs.constructor_arguments);
+  const ContractFactory = await hre.ethers.getContractFactory(configs.contract_name);
+  const contract = await ContractFactory.deploy(...configs.constructor_arguments);
   console.log('Deploy transaction is: ' + contract.deployTransaction.hash)
   await contract.deployed();
   console.log("Contract deployed to:", contract.address);
